f48536a85bdee6a8090ded81d1d975ec89608304
Vinicius Alves <viniciusalvesms757@gmail.com>
2024-08-01 19:43:41 -0300

    fix: database updated & git ignore

--- src/main/java/com/course/web_users_api/Controller/UserController.java
+++ src/main/java/com/course/web_users_api/Controller/UserController.java
@@ -5,6 +5,7 @@ import com.course.web_users_api.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,30 +13,36 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @GetMapping
+    public String homePage() {
+        return "API Home Page";
+    }
+
+    @GetMapping("/users")
+    @PreAuthorize("hasRole('managers', 'users')")
     public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("users/{id}")
     public ResponseEntity<User> getUserById(@PathVariable Long id) {
         Optional<User> user = userService.getUserById(id);
         return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @PostMapping
+    @PostMapping("users")
     public ResponseEntity<User> createUser(@RequestBody User user) {
         User savedUser = userService.saveUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
     }
 
-    @PutMapping("/{id}")
+    @PutMapping("users/{id}")
     public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
         try {
             User updatedUser = userService.putUser(id, user);
@@ -45,13 +52,19 @@ public class UserController {
         }
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("users/{id}")
     public ResponseEntity<User> deleteUser(@PathVariable Long id) {
         if(!userService.getUserById(id).isPresent()) {
             return ResponseEntity.notFound().build();
         }
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("managers")
+    @PreAuthorize("hasRole('managers')")
+    public String managers() {
+        return "Authorized Manager";
     }
 
 }
